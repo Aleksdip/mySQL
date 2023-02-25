759e54cbeaa357dfdbeec1bcf5018c5ff6448081
Aleksdip <118010718+Aleksdip@users.noreply.github.com>
2023-02-25 14:27:52 +0200

    Added nested quer

--- joins 1.sql
+++ joins 1.sql
@@ -14,3 +14,20 @@ on name_number.id = info.id
 join post_salary
 on name_number.id = post_salary.id
 where post = 'Manager';
+
+---------------------------------------------------
+select *,
+(select place from info where name_number.id = info.id) as place
+from name_number;
+ 
+ select birth,
+ (select name from name_number where id = info.id) as name,
+ (select number from name_number where id = info.id) as number
+ from info
+ where status = 'not married';
+ 
+ select birth,
+ (select name from name_number where id = info.id) as name,
+ (select number from name_number where id = info.id) as number
+ from info
+ where (select id from post_salary where id = info.id and post = 'Manager')
